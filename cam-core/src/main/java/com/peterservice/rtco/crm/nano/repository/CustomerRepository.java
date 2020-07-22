62e388f038650caafb9a80a64e51e961d4c9ca5f
Rybachek, Mariya <Mariya.Rybachek@nexign.com>
2020-07-22 17:16:03 +0700

    Split project into 2 modules (cam-api and cam-core)

--- cam-core/src/main/java/com/peterservice/rtco/crm/nano/repository/CustomerRepository.java
+++ cam-core/src/main/java/com/peterservice/rtco/crm/nano/repository/CustomerRepository.java
@@ -1,11 +1,9 @@
 package com.peterservice.rtco.crm.nano.repository;
 
 import com.peterservice.rtco.crm.nano.domain.CustomerEntity;
-import com.peterservice.rtco.crm.nano.dto.CustomerDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
