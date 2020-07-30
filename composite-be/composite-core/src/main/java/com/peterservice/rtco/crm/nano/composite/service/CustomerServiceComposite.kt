62eb583186d18bd0d9d1c24d9a8e0a0e1760d93e
Rybachek, Mariya <Mariya.Rybachek@nexign.com>
2020-07-30 14:33:13 +0700

    В сервисе добавлен метод для получения всех покупателей

--- composite-be/composite-core/src/main/java/com/peterservice/rtco/crm/nano/composite/service/CustomerServiceComposite.kt
+++ composite-be/composite-core/src/main/java/com/peterservice/rtco/crm/nano/composite/service/CustomerServiceComposite.kt
@@ -1,13 +1,14 @@
 package com.peterservice.rtco.crm.nano.composite.service
 
-import com.peterservice.rtco.crm.nano.cam.CamCustomerApiFeign
-import com.peterservice.rtco.crm.nano.cam.dto.CustomerDto
 import com.peterservice.rtco.crm.nano.composite.dto.dto.CustomerDtoComposite
 
 interface CustomerServiceComposite {
+
     fun getById(id: Long): CustomerDtoComposite
 
     fun getByName(name: String):CustomerDtoComposite
+
+    fun getAll(limit: Int, offset: Int): List<CustomerDtoComposite>
 
     fun update(id: Long, customerDtoComposite:CustomerDtoComposite ): CustomerDtoComposite
 
--- composite-be/composite-core/src/main/java/com/peterservice/rtco/crm/nano/composite/service/CustomerServiceImpComposite.kt
+++ composite-be/composite-core/src/main/java/com/peterservice/rtco/crm/nano/composite/service/CustomerServiceImpComposite.kt
@@ -1,6 +1,6 @@
 package com.peterservice.rtco.crm.nano.composite.service
 
-import com.peterservice.rtco.crm.nano.cam.CamCustomerApiFeign
+import com.peterservice.rtco.crm.nano.cam.CustomerApi
 import com.peterservice.rtco.crm.nano.composite.dto.dto.CustomerDtoComposite
 import com.peterservice.rtco.crm.nano.composite.service.extensions.toCustomerDto
 import com.peterservice.rtco.crm.nano.composite.service.extensions.toCustomerDtoComposite
@@ -10,30 +10,36 @@ import org.springframework.stereotype.Service
 @Service
 class CustomerServiceImpComposite (
         @Autowired
-        private val camCustomerApiFeign: CamCustomerApiFeign)
-    : CustomerServiceComposite
+        private val customerApi: CustomerApi
+) : CustomerServiceComposite
 {
-    override fun getById(id: Long) = camCustomerApiFeign
+    override fun getById(id: Long) = customerApi
             .getCustomerById(id)
             .toCustomerDtoComposite()
 
-    override fun getByName(name: String) = camCustomerApiFeign
+    override fun getByName(name: String) = customerApi
             .getCustomerByName(name)
             .toCustomerDtoComposite()
 
+    override fun getAll(limit: Int, offset: Int): List<CustomerDtoComposite> {
+        val customers = customerApi.getAllCustomers(limit, offset)
+        return customers.map { it.toCustomerDtoComposite() }
+        //return customers.map(CustomerDto::toCustomerDtoComposite)
+    }
+
     override fun update(id: Long, customerDtoComposite: CustomerDtoComposite): CustomerDtoComposite {
         val customer = customerDtoComposite.toCustomerDto()
-        val updatedCustomer = camCustomerApiFeign.updateCustomer(id, customer)
+        val updatedCustomer = customerApi.updateCustomer(id, customer)
         return updatedCustomer.toCustomerDtoComposite()
     }
 
     override fun create(customerDtoComposite: CustomerDtoComposite): CustomerDtoComposite {
         val customer = customerDtoComposite.toCustomerDto()
-        val createCustomer = camCustomerApiFeign.createCustomer(customer)
+        val createCustomer = customerApi.createCustomer(customer)
         return createCustomer.toCustomerDtoComposite()
     }
 
     override fun delete (id: Long) {
-        camCustomerApiFeign.deleteCustomer(id)
+        customerApi.deleteCustomer(id)
     }
 }
