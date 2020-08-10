e9a488e377b112ff692bd919d6ab8b42a501eb84
Daniil.Makarov <Daniil.Makarov@nexign.com>
2020-08-11 01:41:09 +0700

    SFA: -настройка тега build модуля sfa-be

---
+++ composite-be/composite-core/src/test/java/com/peterservice/rtco/crm/nano/composite/DictionaryApiFeignTest.java
@@ -0,0 +1,13 @@
+package com.peterservice.rtco.crm.nano.composite;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class DictionaryApiFeignTest {
+
+}
